0da80ffc885a304bf39e654390923a1eaea4b22c
Diego Collares <diegocollares@brq.com>
2020-03-08 19:58:13 -0300

    correction name and main widget

--- lib/main.dart
+++ lib/main.dart
@@ -3,27 +3,28 @@ import 'package:conversor_moeda/screens/home.dart';
 
 import 'package:sentry/sentry.dart';
 
-final SentryClient sentry = new SentryClient(dsn: 'https://b684a4a9acaf4eeab9e1b98fc5741d53@sentry.io/4054692');
+final SentryClient sentry = new SentryClient(
+    dsn: 'https://b684a4a9acaf4eeab9e1b98fc5741d53@sentry.io/4054692');
 
-void main() async {
-  try {
-    runApp(MaterialApp(
-      home: Home(),
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Maino App',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
           hintColor: Colors.black,
           primaryColor: Colors.white,
           inputDecorationTheme: InputDecorationTheme(
             enabledBorder:
-            OutlineInputBorder(borderSide: BorderSide(color: Colors.black)),
+                OutlineInputBorder(borderSide: BorderSide(color: Colors.black)),
             focusedBorder:
-            OutlineInputBorder(borderSide: BorderSide(color: Colors.black)),
+                OutlineInputBorder(borderSide: BorderSide(color: Colors.black)),
             hintStyle: TextStyle(color: Colors.black),
           )),
-    ));
-  } catch(error, stackTrace) {
-    await sentry.captureException(
-      exception: error,
-      stackTrace: stackTrace,
+      home: Home(),
     );
   }
 }
