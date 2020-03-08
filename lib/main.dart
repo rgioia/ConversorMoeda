539d17085407b3b63ff3520c70699c49b00f6d31
Rafael Gióia <rafaelbgioia@gmail.com>
2020-03-08 18:30:28 -0300

    Ajustes icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:conversor_moeda/screens/home.dart';
 
+import 'package:sentry/sentry.dart';
+
+final SentryClient sentry = new SentryClient(dsn: 'https://b684a4a9acaf4eeab9e1b98fc5741d53@sentry.io/4054692');
+
 void main() async {
-  runApp(MaterialApp(
-    home: Home(),
-    theme: ThemeData(
-        hintColor: Colors.black,
-        primaryColor: Colors.white,
-        inputDecorationTheme: InputDecorationTheme(
-          enabledBorder:
-              OutlineInputBorder(borderSide: BorderSide(color: Colors.black)),
-          focusedBorder:
-              OutlineInputBorder(borderSide: BorderSide(color: Colors.black)),
-          hintStyle: TextStyle(color: Colors.black),
-        )),
-  ));
+  try {
+    runApp(MaterialApp(
+      home: Home(),
+      theme: ThemeData(
+          hintColor: Colors.black,
+          primaryColor: Colors.white,
+          inputDecorationTheme: InputDecorationTheme(
+            enabledBorder:
+            OutlineInputBorder(borderSide: BorderSide(color: Colors.black)),
+            focusedBorder:
+            OutlineInputBorder(borderSide: BorderSide(color: Colors.black)),
+            hintStyle: TextStyle(color: Colors.black),
+          )),
+    ));
+  } catch(error, stackTrace) {
+    await sentry.captureException(
+      exception: error,
+      stackTrace: stackTrace,
+    );
+  }
 }
